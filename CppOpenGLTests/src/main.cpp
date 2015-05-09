b8e5e00112aac57546beaa9d3fda655b9ed688f5
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-05-09 15:51:51 -0400

    wat

--- CppOpenGLTests/src/main.cpp
+++ CppOpenGLTests/src/main.cpp
@@ -2,10 +2,16 @@
 		 Title: main.cpp
 		 Programmer: hugo
 		 Date of creation: May 9, 2015
-		 Description: 
+		 Description:
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
 
 
 
 
-
+int main(){
+	std::cout << "Hello world";
+	return 0;
+}
